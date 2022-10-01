5c6a2fa4e83db368cd49ea90adfb5d52411d4108
robinson <robinpaulista85@gmail.com>
2022-10-01 14:26:01 -0300

    Realizando multiplicações com ponteiros de ponteiros

--- ponteiros/195.multiplicacao-de-ponteiro.c
+++ ponteiros/195.multiplicacao-de-ponteiro.c
@@ -1,8 +1,6 @@
 /*
     Aula 195: Exercício 4
 
-    
-
     4) Considere a seguinte declaração: int A, *B, **C, ***D. Escreva um programa que leia a variável 
     A e calcule e exiba o dobro, o triplo e o quádruplo desse valor utilizando apenas os ponteiros B, 
     C e D. O ponteiro B deve ser usada para calcular o dobro, C o triplo e D o quádruplo.
