d84517e85aa8e164845ded39924b5a936d1b2ed4
robinson <robinpaulista85@gmail.com>
2022-10-01 11:32:02 -0300

    Imprimindo endereço e conteúdo de cada posição de um vetor.

---
+++ ponteiros/194.imprmir-endereco-conteudo.c
@@ -0,0 +1,19 @@
+/*
+    Aula 194: Exercício 3
+
+    3) Crie um programa que contenha um array de inteiros com 10 elementos. Imprima o elemento e o 
+       endereço de cada posição do array.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int i, vet[10] = {0,1,2,3,4,5,6,7,8,9};
+
+    for(i = 0; i < 10; i++)
+        printf("Endereco: %p\tConteudo: %d\n", vet + i, *(vet + i));
+
+    return 0;
+}
